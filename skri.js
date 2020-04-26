e152d2326051d245cd72310817f8bad4cbcbb178
Nuuttu <34184542+Nuuttu@users.noreply.github.com>
2020-04-26 14:15:29 +0300

    pp

--- skri.js
+++ skri.js
@@ -5,12 +5,19 @@ function alustus(){
     ruudut += "<table id='lauta'>";
     for (i=1; i<9; i++){
         ruudut += "<tr>";
+        
         for (j=1; j<9; j++){
-            ruudut += "<td>"+j+"</td>";
+            ruudut += "<td"
+            if(j%2 == 0 && i%2 != 0 || j%2 != 0 && i%2 == 0){
+                ruudut+= " class='musta'>";
+            }else{
+                ruudut +=" class='valkoinen'>";
+            }
+            ruudut += j +"</td>";
         }
         ruudut += "</tr>";
     }
     ruudut += "</table>";
     lauta.innerHTML = ruudut;
-    console.log(ruudut);
+   console.log(ruudut);
 }
