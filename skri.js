403e2052710b02810dd73dc1e3a29b1f93d4b1f4
Nuuttu <34184542+Nuuttu@users.noreply.github.com>
2020-04-26 14:38:27 +0300

    koordinaatit

--- skri.js
+++ skri.js
@@ -1,10 +1,11 @@
 function alustus(){
     var lauta = document.getElementById("alusta");
     var ruudut = "";
+    var aakkoset = ["A","B","C","D","E","F","G","H"];
 
     ruudut += "<table id='lauta'>";
-    for (i=1; i<9; i++){
-        ruudut += "<tr>";
+    for (i=8; i>0; i--){
+        ruudut += "<tr><th class='koordinaatti'>"+i+"</th>";
         
         for (j=1; j<9; j++){
             ruudut += "<td"
@@ -13,10 +14,15 @@ function alustus(){
             }else{
                 ruudut +=" class='valkoinen'>";
             }
-            ruudut += j +"</td>";
+            ruudut += aakkoset[j-1] + ""+ i +"</td>";
         }
         ruudut += "</tr>";
     }
+    ruudut += "<tr><th></th>"
+        for (k=1; k<9; k++){
+            ruudut += "<th class='koordinaatti'>"+aakkoset[k-1]+"</th>";
+        }
+        ruudut += "</tr>";
     ruudut += "</table>";
     lauta.innerHTML = ruudut;
    console.log(ruudut);
